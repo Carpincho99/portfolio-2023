d558d84ce4bab2235bc5e973424f318d6f6c6902
Carpincho99 <ezegiorgis@hotmail.com>
2023-01-17 01:26:17 -0300

    add three js and test carpincho model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Model } from './components/Carpincho3D/Carpincho3D'
 
 function App() {
-
   return (
-    <div>
+    <div className="w-screen h-screen">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[5, 5, 5]} />
+        <Model />
+        <OrbitControls />
+      </Canvas>
     </div>
   )
 }
