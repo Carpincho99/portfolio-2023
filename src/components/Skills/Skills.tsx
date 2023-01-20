b218c06d0f6561568cbe2ecb9794a9d4f8a8c52e
Carpincho99 <ezegiorgis@hotmail.com>
2023-01-20 14:07:21 -0300

    init tab skilss

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,10 +2,13 @@
 const Skills = () => {
   return (
     <>
+      <h2 className="font-extrabold text-3xl overflow-hidden dark:text-white">Skills</h2>
       <div className="flex flex-row w-full justify-start items-center dark:text-white">
-        <h2 className="font-extrabold text-3xl overflow-hidden">Skills</h2>
-        <p></p>
-      </div><div>
+        <p className="w-1/2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        <div className="w-1/2 relative h-12 grid grid-cols-3 items-center mx-3 px-3 rounded-full overflow-hidden shadow-2xl bg-[#000D14]">
+        <button className="relative block h-10 px-6 rounded-full">
+        </button>
+        </div>
       </div>
     </>
   )
