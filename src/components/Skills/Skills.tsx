ed3b169245f8193432bdf793eb158d6aa04d5853
Carpincho99 <ezegiorgis@hotmail.com>
2023-01-20 23:15:17 -0300

    add responsiveness to skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -14,66 +14,77 @@ import Vite from './svgIcons/Vite'
 import Linux from './svgIcons/Linux'
 import Webpack from './svgIcons/Webpack'
 import Saas from './svgIcons/Saas'
+import Figma from './svgIcons/Figma'
 
 const Skills = () => {
-
   const SkillTitles = [
     {
-      id: "basic",
-      title: "Basics",
+      title: 'Basics',
       icons: [Html, Css, Js, Typescript],
-      iconsTitle: ["Html", "Css", "Javascript", "Typescript"]
+      iconsTitle: ['Html', 'Css', 'Javascript', 'Typescript'],
     },
     {
-      id: "fontFrameworks",
-      title: "FrontEnd Frameworks",
+      title: 'FrontEnd Frameworks',
       icons: [React, Svelte],
-      iconsTitle: ["ReactJs", "Svelte"]
+      iconsTitle: ['ReactJs', 'Svelte'],
     },
     {
-      id: "cssFrameworks",
-      title: "Css Frameworks",
+      title: 'Css Frameworks',
       icons: [Tailwind, Bootstrap, Material, Saas],
-      iconsTitle: ["Tailwind", "Bootstrap", "Material Ui", "Saas"]
+      iconsTitle: ['Tailwind', 'Bootstrap', 'Material Ui', 'Saas'],
     },
     {
-      id: "misc",
-      title: "Miscellaneous",
-      icons: [Three, Git, Vite,Webpack, Linux],
-      iconsTitle: ["ThreeJs", "Git", "Vite", "WebPack", "Linux"]
-    }
+      title: 'Miscellaneous',
+      icons: [Three, Git, Vite, Webpack, Linux, Figma],
+      iconsTitle: ['ThreeJs', 'Git', 'Vite', 'WebPack', 'Linux', 'Figma'],
+    },
   ]
 
   return (
-    <div className="mt-5 flex flex-row w-full justify-center items-start">
-      <div className="w-1/2">
-        <h2 className="text-transparent bg-clip-text bg-[#F77F00] font-extrabold text-5xl overflow-hidden">Skills</h2>
-        <p className="mt-5 pr-4 text-lg dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+    <div className="mt-5 flex flex-col md:flex-row w-full justify-center items-start">
+      <div className="w-full md:w-1/2">
+        <h2 className="text-transparent bg-clip-text bg-[#F77F00] font-extrabold text-5xl overflow-hidden">
+          Skills
+        </h2>
+        <p className="mt-5 pr-0 md:pr-4 text-lg dark:text-white">
+          I mainly use ReactJs and Tailwind to build beautiful website, like this portfolio. Also, I
+          am learning Svelte (which is an awesome framework!). Some other tools that I am constantly
+          working with are Git/GitHub (for version control) and Linux, like my daily OS.
+        </p>
       </div>
-      <div className={`${styles.skillTitle} w-3/4 drop-shadow-2xl dark:text-white flex flex-col items-center dark:bg-[#000D14] bg-[#EAE2B7]  rounded-[1rem]`}>
-        {
-          SkillTitles.map((section, i) => {
-            return (
-              <>
-                <h2 className={`${i === 0 ? "mt-5" : "mt-0"} pl-6 w-full relative text-start font-extrabold text-2xl overflow-hidden`}>{section.title}</h2>
-                <div className="pl-4 flex flex-row">
-                  {
-                    section.icons.map((Icon, i) => {
-                      return (
-                        <div className={`${i === 0 ? "mr-3 ml-0" : "mx-6"} flex flex-col justify-center items-center my-5`}>
-                          <div className="w-16">
-                            <Icon />
-                          </div>
-                          <p className="dark:text-white mt-1 text-xs">{section.iconsTitle[i]}</p>
-                        </div>
-                      )
-                    })
-                  }
-                </div>
-              </>
-            )
-          })
-        }
+      <div
+        className={`${styles.skillTitle} w-full md:w-3/4 mt-5 md:mt-0 drop-shadow-2xl
+        dark:text-white flex flex-col items-center dark:bg-[#00141F] bg-[#EAE2B7] rounded-[1rem]`}
+      >
+        {SkillTitles.map((section, i) => {
+          return (
+            <>
+              <h2
+                className={`${
+                  i === 0 ? 'mt-5' : 'mt-0'
+                } pl-2 md:pl-6 w-full relative text-start font-extrabold text-2xl overflow-hidden`}
+              >
+                {section.title}
+              </h2>
+              <div className="pl-0 md:pl-4 flex flex-row flex-wrap items-center justify-center">
+                {section.icons.map((Icon, i) => {
+                  return (
+                    <div
+                      className={`${
+                        i === 0 ? 'md:mr-3 md:ml-0 mx-6' : 'mx-6'
+                      } flex flex-col justify-center items-center my-5`}
+                    >
+                      <div className="w-16">
+                        <Icon />
+                      </div>
+                      <p className="dark:text-white mt-1 text-xs">{section.iconsTitle[i]}</p>
+                    </div>
+                  )
+                })}
+              </div>
+            </>
+          )
+        })}
       </div>
     </div>
   )
