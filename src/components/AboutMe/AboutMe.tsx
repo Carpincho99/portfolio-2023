40025d99b8a6f7a25c57d9766e0cf96ce4d31477
Carpincho99 <ezegiorgis@hotmail.com>
2024-01-26 00:25:54 -0300

    complete educ section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -11,13 +11,13 @@ const AboutMe = () => {
         About Me
       </motion.h2>
       <div className="w-full flex items-center justify-center">
-        <div className="w-[900px] mb-8 mt-24 ">
+        <div className="w-[900px] mb-8 mt-24 md:inline flex items-center justify-center flex-col">
           <div className={`${styles.img} mr-12 md:float-left w-[320px] h-[320px] flex items-center justify-center rounded-full overflow-hidden`}>
             <div className="w-[305px] h-[305px] bg-[#EAE2B7] overflow-hidden rounded-full relative">
               <img src={profilePic} alt="" className="w-[320px] absolute top-[-65px] left-[10px] z-50" />
             </div>
           </div>
-          <div className="text-xl">
+          <div className="text-xl md:inline">
             <p className="pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>
           </div>
--- src/components/Educ/Educ.tsx
+++ src/components/Educ/Educ.tsx
@@ -12,19 +12,23 @@ const Educ = () => {
       </motion.h2>
       <div className={`${styles.timelineItems} relative w-full px-0 md:px-14 flex flex-col justify-center items-center mt-5`}>
         {educTimelineElements.map((element) => (
-          <div key={element.id} className={`${element.id % 2 ? 'pr-[50%]' : 'pl-[50%]'} w-full`}>
+          <div key={element.id} className={`${element.id % 2 ? 'pr-0 md:pr-[50%] pl-8 md:pl-0' : 'pl-8 md:pl-[50%]'} w-full mt-5 md:mt-0`}>
             <div className={`${element.id % 2 ? 'pr-0 md:pr-[30px]' : 'pl-0 md:pl-[30px]'} w-full realtive`}>
               <div className="flex flex-row items-center mb-2">
-                <div className={`${element.id % 2 ? 'bg-[#D62828]' : 'bg-[#D62828]'} w-[16px] h-[16px] rounded-full absolute left-[calc(50%-8px)]`}></div>
-                <div className={`${element.id % 2 ? 'justify-end text-[#D62828]' : 'justify-start text-[#D62828]'} w-full flex text-lg font-semibold`}>
+                <div className={`${element.id % 2 ? 'bg-[#D62828]' : 'bg-[#D62828]'} w-[16px] h-[16px] rounded-full absolute left-[1px] md:left-[calc(50%-8px)]`}></div>
+                <div className={`${element.id % 2 ? 'md:justify-end text-[#D62828]' : 'md:justify-start text-[#D62828]'} justify-start w-full flex text-lg font-semibold`}>
                   {element.date}
                 </div>
               </div>
-              <div className="rounded-[0.5rem] p-3 drop-shadow-2xl bg-[#EAE2B7] dark:bg-[#00141F]">
-                <div className="font-bold text-xl">{element.title}</div>
-                <div>{element.location}</div>
-                <div>{element.description}</div>
-              </div>
+              <motion.div initial={{ x: `${screen.width > 768 ? element.id % 2 ? "-25vw" : "25vw" : "50vw"}` }} whileInView={{ x: 0 }} transition={{ duration: 0.5 }} viewport={{ once: true }}
+                className="rounded-[0.5rem] p-3 drop-shadow-2xl bg-[#EAE2B7] dark:bg-[#00141F]"
+              >
+                <div className="w-full flex flex-row items-center justify-between">
+                  <div className="font-bold text-xl dark:text-white">{element.title}</div>
+                  <div className="dark:text-white">{element.location}</div>
+                </div>
+                <div className="pt-2 dark:text-white">{element.description}</div>
+              </motion.div>
             </div>
           </div>
         ))}
